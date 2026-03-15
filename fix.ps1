abe28d4a77acd171a34fbea29de22d407d21be97
AminSassi <aminsassi267@gmail.com>
2026-03-15 03:23:48 +0100

    chore: remove temp fix.ps1 and chunk.txt

--- fix.ps1
+++
@@ -1,4 +0,0 @@
-$content = [System.IO.File]::ReadAllText('index.html')
-$idx = $content.IndexOf('feedbackStack')
-$chunk = $content.Substring($idx, 2500)
-[System.IO.File]::WriteAllText('chunk.txt', $chunk)
